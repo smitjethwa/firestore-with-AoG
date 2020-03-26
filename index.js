b84619c977058ba26b4ceb9a1b5e94d1a277d043
smitjethwa <smitjethwa20@gmail.com>
2020-03-26 15:43:59 +0530

    webhook added

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const functions = require('firebase-functions');
+const {WebhookClient} = require('dialogflow-fulfillment');
+const admin = require('firebase-admin');
+admin.initializeApp();
+const db = admin.firestore();
+
+process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  const agent = new WebhookClient({ request, response });
+
+  function getNameHandler(agent) {
+    let name = agent.parameters.name;
+    let email = agent.parameters.email;
+    db.collection("userDetails").add({ 
+      name: name,
+      email: email
+    });
+    agent.add('Data added successfully!!');
+  }
+
+  let intentMap = new Map();
+  intentMap.set('setDetails', getNameHandler);
+  agent.handleRequest(intentMap);
+});
